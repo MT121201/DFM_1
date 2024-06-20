9910448c83f85e9544b8870ff388811acace72cf
MT121201 <triet.kieu_binu@hcmut.edu.vn>
2024-06-20 17:53:29 +0700

    last edit

--- Backbone/Attention/LinearAttention.py
+++ Backbone/Attention/LinearAttention.py
@@ -1,5 +1,27 @@
 import torch.nn as nn 
 import torch 
-
+from einops import rearrange
 class LinearAttention(nn.Module):
-    pass
+    def __init__(self, dim, 
+                 heads = 8, 
+                 dim_head = 64):
+        super().__init__()
+        inner_dim = dim_head * heads
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.to_qkv = nn.Conv2d(dim, inner_dim * 3, 1, bias = False)
+        self.to_out = nn.Conv2d(inner_dim, dim, 1)
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        qkv = self.to_qkv(x).chunk(3, dim = 1)
+        q,k,v = map(lambda t: rearrange(t, 'b (h c) x y -> b h c (x y)', h = self.heads), qkv)
+        q *= self.scale
+
+        k = k.softmax(dim = -2)
+        context = torch.einsum('b h d n, b h e n -> b h d e', k, v)
+
+        out = torch.einsum('b h d e, b h d n -> b h e n', q, context)
+        out = rearrange(out, 'b h c (x y) -> b (h c) x y', h = self.heads, x = h, y = w)
+        return self.to_out(out)
